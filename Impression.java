225da2f9438c609053fc3c9ab4df3a6e870e53a1
dthcas <56852554+dthcas@users.noreply.github.com>
2019-11-26 10:33:18 +0800

    Create Impression.java
    Initial creation of the Impression class file

---
+++ Impression.java
@@ -0,0 +1,15 @@
+/* Impression
+
+Variables:
+•	int id
+•	double interesting
+•	double attractive
+•	double kind
+•	double chemistry
+
+Methods:
+•	public double getImpression() – This method can synthesize the results of the different values to give an overall impression of the person from 0 to 2
+•	public void setInteresting(double i)
+•	public void setAttractive(double a)
+•	public void setKind(double k)
+*/
