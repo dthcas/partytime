02380aaa57ab8449934bbdb0841b2af2a513ea76
dthcas <56852554+dthcas@users.noreply.github.com>
2019-11-26 10:29:14 +0800

    Create Conversation.java
    Initial commit of the Conversation file

---
+++ Conversation.java
@@ -0,0 +1,15 @@
+/* Conversation
+
+Variables:
+•	int ID
+•	boolean status – for open or closed conversation
+•	Person p1
+•	Person p2
+•	int length
+•	Topic [] topics
+
+Methods:
+•	public Topic prompt(Person p) – asks a person to return a topic of conversation, which is returned
+•	public void beginConversation() – starts a conversation
+•	public void endConversation() – terminates a conversation
+*/
