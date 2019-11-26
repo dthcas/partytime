05142be102b19a5fdea2ad0af086271c1758acbc
dthcas <56852554+dthcas@users.noreply.github.com>
2019-11-26 10:27:32 +0800

    Create Topic.java
    Initial creation of the Topic class

---
+++ Topic.java
@@ -0,0 +1,13 @@
+/* Topic
+
+Variables:
+•	String name
+•	double interest
+•	double importance
+
+Methods:
+•	public String getName()
+•	public double getInterest()
+•	public void setInterest(double i)
+•	public void setImportance(double i)
+*/
