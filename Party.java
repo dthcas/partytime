62abb217f2eb99c8bac1c65315bfc17c95ec57be
dthcas <56852554+dthcas@users.noreply.github.com>
2019-11-26 10:24:49 +0800

    Create Party.java
    Initial commit for the file which will serve as a placeholder

---
+++ Party.java
@@ -0,0 +1,15 @@
+/* New file for Party class */
+/* Variables:  
+•	double start_time
+•	datetime end_time
+•	int attendees
+•	Person [] guests
+•	Conversation [] conversations
+•	Music [] playlist
+Methods:
+•	public void startParty() – method which launches the party and starts the playlist of music, puts guests into their first positions.
+•	public void endParty() – finish the party and print out the results.
+•	private boolean changeMusic() – changes the song and switches up the conversations so that people can have a chance to talk to someone new (unless they are really enjoying their conversation!)
+•	public int getGuests() – returns the number of guests at the party
+•	public boolean addGuest(Person p) – adds a guest to the party.  Returns true if the guest has been added to the party, false if the party is full.
+*/
