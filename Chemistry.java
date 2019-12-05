a7b060a52c907238eafbed1a2e93e7d95958aa96
David <David@DARKNESS>
2019-12-05 19:47:32 +0800

    Changed comments slightly

--- Chemistry.java
+++ Chemistry.java
@@ -1,7 +1,6 @@
-/* Chemistry
-Add some comments here
-public static double getChemistry(Person p1, Person p2) returns the chemistry value for two people, based on some mystical, magical quality that the universe decides.  Value between 0 and 2.
-
+/* 	Chemistry
+	Add some comments here
+	public static double getChemistry(Person p1, Person p2) returns the chemistry value for two people, based on some mystical, magical quality that the universe decides.  Value between 0 and 2.
 */
 
 public class Chemistry {
