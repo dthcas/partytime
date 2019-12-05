13f796c0f72631a2ac7840b5c9f9a3b29ed84f34
David <56852554+dthcas@users.noreply.github.com>
2019-12-05 19:31:48 +0800

    Added basic class code and constructor

--- Chemistry.java
+++ Chemistry.java
@@ -1,5 +1,12 @@
 /* Chemistry
 Add some comments here
-public static double getChemistry(Person p1, Person p2) – returns the chemistry value for two people, based on some mystical, magical quality that the universe decides.  Value between 0 and 2.
+public static double getChemistry(Person p1, Person p2) returns the chemistry value for two people, based on some mystical, magical quality that the universe decides.  Value between 0 and 2.
 
 */
+
+public class Chemistry {
+	
+	public Chemistry() {
+		
+	}
+}
