f76c8dd13ec9acfe70200075f0604aa0ba1864bd
beckham921219 <beckham921219@172.16.99.157>
2019-12-09 11:21:56 +0800

    I added music class with a constructor for each music attributes.

--- partytime/Music.java
+++ partytime/Music.java
@@ -1,16 +1,19 @@
-package partytime;
-
 /* Music will define a class of song which will be played at a party */
 
 // Music has an artist, title and running time.
 // Methods will be: 
-// 	getNextSong() which will return a new song
+// 	getNextSong() which will a return a new song
 //  getPlaylist(int numsongs) which will return a list of songs
 //  importMusic() which will scan an input file for songs and store them for use during operation
+import java.time.LocalDate;
 
 public class Music {
-	
-	public Music() {
-		
+	private String artist, title;
+	private Duration duration;
+
+	public Music(String artist, String title, int minDuration, int secDuration) {
+		this.artist = artist;
+		this.title = title;
+		this.duration = Duration.of((secDuration + minDuration*60), ChronoUnit.SECONDS);
 	}
 }
