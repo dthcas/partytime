364d1555aa88addc2e42ec3df9f6c814459dbff4
Warren Wang <crimsonpython24@gmail.com>
2019-12-09 16:21:54 +0800

    Update People_Generator.py
    Ensure that the `charisma` column has range 0 to 2. Was initially out of range b/c it was not in a loop due to additional comma issues.
    Extracted variables.
    Added languages for variety.

--- partytime/imports/People_Generator.py
+++ partytime/imports/People_Generator.py
@@ -2,7 +2,7 @@ from faker import Faker
 import random
 
 f = open("people.txt", "w")
-fake = Faker(['en_US', 'en_AU', 'en_CA', 'en_GB', 'en_NZ'])
+fake = Faker(['en_US', 'en_AU', 'en_CA', 'en_GB', 'en_NZ', 'es_ES', 'fr_FR'])
 f.write('ID,Name,Age,Humor,Empathy,Attractiveness,Intelligence,Charisma\n')
 
 c = int(input("how many profiles do you want? "))
@@ -10,5 +10,6 @@ for _ in range(c):
     f.write(str(_ + 1) + ',' + fake.name() + ',')
     f.write(str(random.randint(18, 65)) + ',')
     for __ in range(4):
-        f.write(str(round(random.random() * 2, 2)) + ',')
-    f.write(str(round(random.random(), 2)) + '\n')
+        str1 = str(round(random.random() * 2, 2))
+        f.write(str1 + ',')
+    f.write(str(round(random.random() * 2, 2)) + '\n')
