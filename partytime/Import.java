041fe5aab5d892c590c02def53b9b10f086db88a
David <David@DARKNESS>
2019-12-09 23:08:34 +0800

    Added music import and get functions which are publicly viewable for grabbing lists

--- partytime/Import.java
+++ partytime/Import.java
@@ -2,6 +2,8 @@ package partytime;
 /* Import.java will allow a class to import various data from CSV files to allow for people, conversations and topics */
 
 import java.io.*;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.io.IOException;
 //import java.nio.charset.StandardCharsets;
@@ -11,16 +13,32 @@ import java.io.IOException;
 
 public class Import {
 	
-	public static Person[] getPeopleFromCSV(int num, String fileName) throws IOException {
+	private static ArrayList<Person> allPeople;
+	private static ArrayList<Topic> allTopics;
+	private static ArrayList<Music> allMusic;
+	
+	public Import() {
+		
+		try {
+			getPeopleFromCSV("imports\\people.csv");
+			getTopicsFromCSV("imports\\topics.csv");
+			getMusicFromCSV("imports\\music.csv");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void getPeopleFromCSV(String fileName) throws IOException {
 		
 		// find the path to the file and create an instance of BufferedReader
-		BufferedReader br = new BufferedReader(new FileReader("imports\\people.csv"));
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
 
 		// create a new list of people for returning to user
-		Person [] pList = new Person[num];
 		// make an arraylist to hold all people from the file.  This way we can 
 		// choose random people from the list instead of the same first few.
-		ArrayList<Person> allPeople = new ArrayList<>();
+		//ArrayList<Person> allPeople = new ArrayList<>();
 		// read the first line from the text file
 		String line = br.readLine();
 
@@ -39,7 +57,7 @@ public class Import {
 			String[] values = line.split(",");
 			if(values.length!=8) {
 				System.out.print("Please check input file "+fileName);
-				return pList;
+				return;
 			}
 			p_id = Integer.parseInt(values[0]);
 			p_age = Integer.parseInt(values[2]);
@@ -50,7 +68,7 @@ public class Import {
 			p_ch = Double.parseDouble(values[7]);
 
 			// adding this new person into ArrayList of all people
-			allPeople.add(new Person(p_age,values[1],p_id,p_hm,p_em,p_at,p_in,p_ch));
+			Import.allPeople.add(new Person(p_age,values[1],p_id,p_hm,p_em,p_at,p_in,p_ch));
 
 			// read next line before looping
 			// if end of file reached, line would be null
@@ -58,14 +76,26 @@ public class Import {
 		}
 
 		br.close(); // close the file reader
-
-		int ap_size = allPeople.size();
+		
+		return;
+	}
+	
+	// Returns a list of people generated from the master list of all people imported.
+	// Does so by making a clone of the master list then removing people from it and adding it to
+	// a regular array.
+	
+	public static Person[] getPeople(int num) {
+		
+		Person [] pList = new Person[num];
+		ArrayList<Person> cloneList = new ArrayList<Person>(allPeople);
+		
+		int ap_size = cloneList.size();
 		int p_index;
         
 		for(int i=0; i<num && i<ap_size; i++) {
 			p_index = (int) (Math.random()*(ap_size-1));
-			pList[i] = allPeople.get(p_index);
-			allPeople.remove(p_index);
+			pList[i] = cloneList.get(p_index);
+			cloneList.remove(p_index);
 			ap_size--;
 		}
 		
@@ -73,13 +103,11 @@ public class Import {
 	}
 
 	
-	public static Topic[] getTopicsFromCSV(int num, String fileName) throws IOException {
+	private void getTopicsFromCSV(String fileName) throws IOException {
 		
 		// find the path to the file and create an instance of BufferedReader
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
 
-		// create a new list of people for returning to user
-		Topic [] tList = new Topic[num];
 		// make an arraylist to hold all people from the file.  This way we can 
 		// choose random people from the list instead of the same first few.
 		ArrayList<Topic> allTopics = new ArrayList<>();
@@ -87,9 +115,12 @@ public class Import {
 		String line = br.readLine();
 
 		// temp person information variables
-		String t_name,t_l1,t_l2,t_l3,t_l4,t_l5,t_l6,t_l7,t_l8,t_l9,t_l10;
+		String t_name;
+		String t_l1,t_l2,t_l3,t_l4,t_l5,t_l6,t_l7,t_l8,t_l9,t_l10;
+		String t_r1,t_r2,t_r3,t_r4,t_r5,t_r6,t_r7,t_r8,t_r9,t_r10;
 		int lineNum=1;
 		String [] t_statements = new String[10];
+		String [] t_replies = new String[10];
 
 		// loop until all lines are read
 		while (line != null) {
@@ -97,19 +128,20 @@ public class Import {
 			if(lineNum++==1) continue; // Ignore the first line and increment the line number
 
 			// use string.split to load a string array with the values from
-			// each line of the file, using a comma as the delimiter
+			// each line of the file, using a comma as the delimiter.
 			String[] values = line.split(",");
 			if(values.length!=11) {
 				System.out.print("Please check input file "+fileName);
-				return tList;
+				return;
 			}
 			t_name = values[0];
-			for(int i=0; i<10; i++) {
-				t_statements[i] = values[i+1];
+			for(int i=0,j=0; i<20; i+=2,j++) {
+				t_statements[j] = values[i+1];
+				t_replies[j] = values[i+2];
 			}
 
-			// adding this new person into ArrayList of all people
-			allTopics.add(new Topic(t_name,t_statements));
+			// add this topic into the master list of all topics
+			Import.allTopics.add(new Topic(t_name,t_statements,t_replies));
 
 			// read next line before looping
 			// if end of file reached, line would be null
@@ -117,12 +149,22 @@ public class Import {
 		}
 
 		br.close(); // close the file reader
+		
+		return;
+	}
+	
+	
+	// Used by other classes to receive a fixed number of Topics in array form
+	public static Topic[] getTopics(int num) {
 
-		int at_size = allTopics.size();
+		Topic [] tList = new Topic[num];
+		ArrayList<Topic> cloneList = new ArrayList<Topic>(allTopics);
+		
+		int at_size = cloneList.size();
 		int t_index;
         
 		for(int i=0; i<num && i<at_size; i++) {
-			t_index = (int) (Math.random()*(ap_size-1));
+			t_index = (int) (Math.random()*(at_size-1));
 			tList[i] = allTopics.get(t_index);
 			allTopics.remove(t_index);
 			at_size--;
@@ -130,5 +172,80 @@ public class Import {
 		
 		return tList;
 	}	
+	
+	// Internal function to import music from a CSV file for use in the party
+	private void getMusicFromCSV(String fileName) throws IOException {
+		
+		// find the path to the file and create an instance of BufferedReader
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+	
+		// create a new list of people for returning to user
+		// make an arraylist to hold all people from the file.  This way we can 
+		// choose random people from the list instead of the same first few.
+		//ArrayList<Person> allPeople = new ArrayList<>();
+		// read the first line from the text file
+		String line = br.readLine();
+	
+		// temp person information variables
+		String artist,title,dur;
+		Duration length = Duration.ZERO;
+		int lineNum=1;
+	
+		// loop until all lines are read
+		while (line != null) {
+	
+			if(lineNum++==1) continue; // Ignore the first line and increment the line number
+	
+			// use string.split to load a string array with the values from
+			// each line of the file, using a comma as the delimiter
+			String[] values = line.split(",");
+			String[] timer;
+			
+			if(values.length!=3) {
+				System.out.print("Please check input file "+fileName);
+				return;
+			}
+			artist = values[0];
+			title = values[1];
+			dur = values[3];
+			timer = dur.split(":");
+			length = Duration.ZERO;
+			length.plusMinutes(Long.parseLong(timer[0]));
+			length.plusSeconds(Long.parseLong(timer[1]));
+	
+			// adding this new person into ArrayList of all people
+			Import.allMusic.add(new Music(artist,title,length));
+	
+			// read next line before looping
+			// if end of file reached, line would be null
+			line = br.readLine();
+		}
+	
+		br.close(); // close the file reader
+		
+		return;
+	}
+	
+	// Returns a list of people generated from the master list of all people imported.
+	// Does so by making a clone of the master list then removing people from it and adding it to
+	// a regular array.
+	
+	public static Music[] getPlaylist(int num) {
+		
+		Music [] mList = new Music[num];
+		ArrayList<Music> cloneList = new ArrayList<Music>(allMusic);
+		
+		int am_size = cloneList.size();
+		int m_index;
+	    
+		for(int i=0; i<num && i<am_size; i++) {
+			m_index = (int) (Math.random()*(am_size-1));
+			mList[i] = cloneList.get(m_index);
+			cloneList.remove(m_index);
+			am_size--;
+		}
+		
+		return mList;
+	}
 	
 }
