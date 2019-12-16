b33a6c6f8ef6cb0a5a2126d0a2f10c8dc11ce622
David <56852554+dthcas@users.noreply.github.com>
2019-12-16 12:43:18 +0800

    Basic file for testing the import functionality (to be deleted later)

---
+++ partytime/Test.java
@@ -0,0 +1,15 @@
+package partytime;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Import data = new Import();
+		
+		System.out.print(data.getPeople(5));
+		
+	}
+
+}
