e7062cc6bf4196fc780e35c08bd0371321907663
David <David@DARKNESS>
2019-12-25 12:46:29 +1100

    Fixed to create correctly via a constructor and then return the next song

--- partytime/Playlist.java
+++ partytime/Playlist.java
@@ -2,20 +2,23 @@ package partytime;
 
 //This is the playlist class for storing music.
 
-public class Playlist{
-   Music[] playlist;
+public class Playlist {
+   private Music[] playlist;
+   private int cur_song;
    
-   public Music[] getPlaylist(int numsongs){
-	   Music[] musicArr = new Music[numsongs];
-	   return musicArr;
+   public Playlist(Music[] m) {
+	   this.playlist = m;
+	   cur_song = 0;
    }
    
-   public Music getNextSong(){
-    
+   public Music getNextSong() {
+   
+	   if(cur_song+1 < this.playlist.length-1) {
+		   cur_song++;
+		   return this.playlist[cur_song];
+	   }
+	   else {
+		   return null;
+	   }
    }
-   
-   public void importMusic(Music music){
-   
-   }
-   
- }
+}
